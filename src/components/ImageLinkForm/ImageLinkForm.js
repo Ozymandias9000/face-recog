98c48d0f163249b0e7e240f1514e0fa157295ec7
Ozymandias9000 <[nick_murphy@sbcglobal.net]>
2018-03-30 12:28:30 -0700

    Added ImageLinkForm component

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import './ImageLinkForm.css'
 
 const ImageLinkForm = () => {
 	return (
-		<div>
-	
+		<div className='ImageLinkForm'>
+			<p>{'This Magic Brain will detect faces!'}</p>
+			<div className='center'>
+				<input type='text' />
+				<button>Detect</button>
+			</div>
 		</div>
 	);
 };
