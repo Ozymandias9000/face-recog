4fce9542d81acac8d59c20375f7d2fb5b4c482db
Ozymandias9000 <[nick_murphy@sbcglobal.net]>
2018-04-15 10:14:03 -0700

    deploy

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -18,7 +18,7 @@ class SignIn extends React.Component {
 	}
 
 onSubmitSignIn = () => {
-	fetch('http://localhost:3000/signin', {
+	fetch('https://git.heroku.com/cryptic-ravine-28032.git/signin', {
 		method: 'post',
 		headers: {'Content-Type': 'application/json'},
 		body: JSON.stringify({
